795d71a0be00ad73ae7412fd74fef8608604b80d
Monika Dabrowska <monikad71@onet.pl>
2023-01-27 21:58:57 +0100

    Solution 16.1

--- src/main/java/pl/javastart/task/Season.java
+++ src/main/java/pl/javastart/task/Season.java
@@ -1,5 +1,20 @@
 package pl.javastart.task;
 
 public enum Season {
-    //uzupełnij
+    SPRING("Wiosna", new String[] {"marzec", "kwiecień", "maj"}),
+    SUMMER("Lato", new String[] {"czerwiec", "lipiec", "sierpień"}),
+    AUTUMN("Jesień", new String[] {"wrzesień", "październik", "listopad"}),
+    WINTER("Zima", new String[] {"grudzień", "styczeń", "luty"});
+
+    private final String seasonNameInPl;
+    final String[] months;
+
+    Season(String seasonNameInPl, String[] months) {
+        this.seasonNameInPl = seasonNameInPl;
+        this.months = months;
+    }
+
+    public String getSeasonNameInPl() {
+        return seasonNameInPl;
+    }
 }
--- src/main/java/pl/javastart/task/SeasonManager.java
+++ src/main/java/pl/javastart/task/SeasonManager.java
@@ -1,5 +1,6 @@
 package pl.javastart.task;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class SeasonManager {
@@ -7,6 +8,25 @@ public class SeasonManager {
     // uzupełnij metodę - do wczytywania danych użyj przekazany Scanner
     public void run(Scanner scanner) {
 
-    }
+        System.out.println("Podaj porę roku:");
+        for (Season season : Season.values()) {
+            System.out.println(season.getSeasonNameInPl());
+        }
+        String userInput = scanner.nextLine();
 
+        System.out.println("W tej porze roku są następujące miesiące:");
+        if (userInput.equalsIgnoreCase(Season.SPRING.getSeasonNameInPl())) {
+            System.out.println(Arrays.toString(Season.SPRING.months));
+
+        } else if (userInput.equalsIgnoreCase(Season.SUMMER.getSeasonNameInPl())) {
+            System.out.println(Arrays.toString(Season.SUMMER.months));
+
+        } else if (userInput.equalsIgnoreCase(Season.AUTUMN.getSeasonNameInPl())) {
+            System.out.println(Arrays.toString(Season.AUTUMN.months));
+
+        } else if (userInput.equalsIgnoreCase(Season.WINTER.getSeasonNameInPl())) {
+            System.out.println(Arrays.toString(Season.WINTER.months));
+        }
+    }
 }
+
